8fbdf038eed02adc3678143f87a930e923c659b8
Peter Williams <peter@newton.cx>
2022-01-07 10:51:49 -0500

    ads_ref_extract/config.py: honor standard ADS environment and type-annotate better

--- ads_ref_extract/config.py
+++ ads_ref_extract/config.py
@@ -3,38 +3,47 @@ A simple configuration abstraction to hopefully make it easy to use these
 modules in both standalone CLIs and the standard Celery environment.
 """
 
+import os
 from pathlib import Path
 
 __all__ = ["Config"]
 
 
 class Config(object):
-    logs_base = None
-    fulltext_base = None
-    target_refs_base = None
-    resolved_refs_base = None
-    abstracts_config_base = None
-    abstracts_links_base = None
+    logs_base: Path = None
+    fulltext_base: Path = None
+    target_refs_base: Path = None
+    resolved_refs_base: Path = None
+    abstracts_config_base: Path = None
+    abstracts_links_base: Path = None
+    tex_bin_dir: Path = None
 
     @classmethod
     def new_defaults(cls):
         """
         Create a new Config with default paths set for ADS infra.
         """
+
+        abstracts = Path(os.environ.get('ADS_ABSTRACTS', '/proj/ads/abstracts'))
+        references = Path(os.environ.get('ADS_REFERENCES', '/proj/ads/references'))
+
         inst = cls()
-        inst.logs_base = Path("/proj/ads/abstracts/sources/ArXiv/log")
+        inst.logs_base = abstracts / "sources" / "ArXiv" / "log"
 
         # NB: this must end in the string `fulltext` in order for some of the
         # log-parsing code to work correctly.
-        inst.fulltext_base = Path("/proj/ads/abstracts/sources/ArXiv/fulltext")
+        inst.fulltext_base = abstracts / "sources" / "ArXiv" / "fulltext"
 
         # NB: These must be the same but with `sources` replaced with
         # `resolved`.
-        inst.target_refs_base = Path("/proj/ads/references/sources")
-        inst.resolved_refs_base = Path("/proj/ads/references/resolved")
+        inst.target_refs_base = references / "sources"
+        inst.resolved_refs_base = references / "resolved"
 
-        inst.abstracts_config_base = Path("/proj/ads/abstracts/config")
-        inst.abstracts_links_base = Path("/proj/ads/abstracts/links")
+        inst.abstracts_config_base = abstracts / "config"
+        inst.abstracts_links_base = abstracts / "links"
+
+        # This assumes that we're running in the standard Docker container:
+        inst.tex_bin_dir = Path("/src/tex/bin/x86_64-linux")
         return inst
 
     def classic_session_log_path(self, session_id):
